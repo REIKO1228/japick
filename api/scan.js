775eebe15b97e6188e9f3cf5c864620d70f0ff1e
REIKO1228 <reikoterakawa1228@gmail.com>
2026-06-25 09:35:14 +0900

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
           }],
           generationConfig: {
             temperature: 0,
-            maxOutputTokens: 64
+            maxOutputTokens: 256
           }
         })
       }
